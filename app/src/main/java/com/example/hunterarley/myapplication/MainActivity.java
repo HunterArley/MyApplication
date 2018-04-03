6625cbba1cb721aca2c3891ce2c8fbbcfeb679c0
hunterarley <18200399036@163.com>
2018-04-03 23:44:19 +0800

    TextView的简单实例

--- app/src/main/java/com/example/hunterarley/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hunterarley/myapplication/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.hunterarley.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView Tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Tv=(TextView) findViewById(R.id.Tv);
+//        Tv.setText("I am an Android Developer");
     }
 }
